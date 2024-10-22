a0274a4d959f50475f8778d4aa43bc12ca3e585a
harishgowda120 <160123683+harishgowda120@users.noreply.github.com>
2024-10-22 19:22:31 +0530

    Add files via upload

---
+++ src/Components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import loading from '../loding.gif'
+
+const Loading=()=>{
+ 
+    return (
+      <div className='text-center'>
+            <img src={loading} alt='Loading Spinner'/>
+      </div>
+    )
+  
+}
+
+export default Loading;
